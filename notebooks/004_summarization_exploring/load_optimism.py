22411a2251e1f9a99717e6216e81209d42e40964
victorsouza89 <69157327+victorsouza89@users.noreply.github.com>
2024-07-09 20:59:02 -0300

    first tests on summarization

---
+++ notebooks/004_summarization_exploring/load_optimism.py
@@ -0,0 +1,203 @@
+import re
+import json
+from typing import Dict, List, Any
+
+from langchain_core.documents.base import Document
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+
+from op_chat_brains.documents import (
+    DocumentProcessingStrategy,
+    DocumentProcessorFactory,
+)
+
+
+class FragmentsProcessingStrategy(DocumentProcessingStrategy):
+    def process_document(self, file_path: str) -> List[Document]:
+        with open(file_path, "r") as f:
+            docs_read = f.read()
+
+        docs_read = re.split(r"==> | <==", docs_read)
+        docs = []
+        path = []
+        for d in docs_read:
+            if "\n" not in d:
+                path = d.split("/")
+            else:
+                docs.append(
+                    {
+                        "path": "/".join(path[:-1]),
+                        "document_name": path[-1],
+                        "content": d,
+                    }
+                )
+
+        docs = [d for d in docs if d["content"].strip() != ""]
+
+        headers_to_split_on = [
+            ("##", "header 2"),
+            ("###", "header 3"),
+            ("####", "header 4"),
+            ("#####", "header 5"),
+            ("######", "header 6"),
+        ]
+        markdown_splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=headers_to_split_on
+        )
+
+        fragments_docs = []
+        for d in docs:
+            f = markdown_splitter.split_text(d["content"])
+            for fragment in f:
+                fragment.metadata["path"] = d["path"]
+                fragment.metadata["document_name"] = d["document_name"]
+                fragments_docs.append(fragment)
+
+        return fragments_docs
+
+    def get_db_name(self) -> str:
+        return "fragments_docs_db"
+
+
+class ForumPostsProcessingStrategy(DocumentProcessingStrategy):
+    @staticmethod
+    def process_document(file_path: str) -> List[Document]:
+        with open(file_path, "r") as file:
+            boards, threads, posts = {}, {}, {}
+            for line in file:
+                data_line = json.loads(line)
+                type_line = data_line["type"]
+                try:
+                    id = data_line["item"]["data"]["id"]
+                    if type_line == "board":
+                        boards[id] = {"name": data_line["item"]["data"]["name"]}
+                    elif type_line == "thread":
+                        threads[id] = {
+                            "title": data_line["item"]["data"]["title"],
+                            "category_id": data_line["item"]["data"]["category_id"],
+                            "created_at": data_line["item"]["data"]["created_at"],
+                            "views": data_line["item"]["data"]["views"],
+                            "like_count": data_line["item"]["data"]["like_count"],
+                        }
+                    elif type_line == "post":
+                        posts[id] = {
+                            "url": data_line["item"]["url"],
+                            "created_at": data_line["item"]["data"]["created_at"],
+                            "username": data_line["item"]["data"]["username"],
+                            "score": data_line["item"]["data"]["score"],
+                            "readers_count": data_line["item"]["data"]["readers_count"],
+                            "moderator": data_line["item"]["data"]["moderator"],
+                            "admin": data_line["item"]["data"]["admin"],
+                            "staff": data_line["item"]["data"]["staff"],
+                            "trust_level": data_line["item"]["data"]["trust_level"],
+                            "content": data_line["item"]["content"],
+                            "creation_time": data_line["item"]["creation_time"],
+                            "path": data_line["item"]["path"],
+                            "download_time": data_line["download_time"],
+                        }
+                except KeyError:
+                    pass
+
+        for id_post, post in posts.items():
+            path = post["path"]
+            try:
+                id_board = int(path[0])
+                post["board_name"] = boards[id_board]["name"]
+                post["board_id"] = id_board
+            except:
+                post["board_name"] = None
+
+            try:
+                id_thread = int(path[1])
+                post["thread_title"] = threads[id_thread]["title"]
+                post["thread_id"] = id_thread
+            except:
+                post["thread_title"] = None
+
+        return posts
+    
+    @staticmethod
+    def return_posts(file_path: str) -> List[Document]:
+        posts_forum = [
+            Document(
+                page_content=d["content"],
+                metadata={
+                    "board_name": d["board_name"],
+                    "thread_title": d["thread_title"],
+                    "creation_time": d["creation_time"],
+                    "username": d["username"],
+                    "moderator": d["moderator"],
+                    "admin": d["admin"],
+                    "staff": d["staff"],
+                    "trust_level": d["trust_level"],
+                    "id": ".".join(d["path"]) + "." + str(id),
+                },
+            )
+            for id, d in ForumPostsProcessingStrategy.process_document(file_path).items()
+        ]
+
+        return posts_forum
+    
+    @staticmethod
+    def return_threads(df_posts) -> List[Document]:
+        threads =[]
+        for t in df_posts['thread_id'].unique():
+            posts_thread = df_posts[df_posts['thread_id'] == t].sort_values(by='created_at')
+            try:
+                url = posts_thread['url'].iloc[0]
+                title = posts_thread['thread_title'].iloc[0]
+                board = posts_thread['board_name'].iloc[0]
+            except:
+                url = None
+                title = None
+                board = None
+            try:
+                str_thread = f"OPTIMISM FORUM \n board: {posts_thread['board_name'].iloc[0]}\n thread: {posts_thread['thread_title'].iloc[0]}\n\n"
+                for i, post in posts_thread.iterrows():
+                    str_thread += f"---\n\nPOST \n user: {post['username']}" 
+                    if post['moderator']:
+                        str_thread += " (moderator)"
+                    if post['admin']:
+                        str_thread += " (admin)"
+                    if post['staff']:
+                        str_thread += " (staff)"
+                    str_thread += f"\n created_at: {post['created_at']} \n trust_level (0-4): {post['trust_level']} \n\n"
+                    str_thread += f"{post['content']}\n\n"
+            except:
+                None
+
+            metadata = {
+                "thread_id": t,
+                "thread_title": title,
+                "board_name": board,
+                "url": url,
+                "num_posts": len(posts_thread),
+            }
+            threads.append([str_thread, metadata])
+
+        threads_forum = [
+            Document(
+                page_content = t[0],
+                metadata = t[1]
+            ) for t in threads
+        ]
+
+        return threads_forum
+
+    def get_db_name(self) -> str:
+        return "posts_forum_db"
+
+
+class OptimismDocumentProcessorFactory(DocumentProcessorFactory):
+    def create_processor(self, doc_type: str) -> DocumentProcessingStrategy:
+        if doc_type == "fragments":
+            return FragmentsProcessingStrategy()
+        elif doc_type == "forum_posts":
+            return ForumPostsProcessingStrategy()
+        else:
+            raise ValueError(f"Unsupported document type: {doc_type}")
+
+    def get_document_types(self) -> Dict[str, str]:
+        return {
+            "fragments": "001-initial-dataset-governance-docs/file.txt",
+            "forum_posts": "002-governance-forum-202406014/dataset/_out.jsonl",
+        }
---
+++ notebooks/004_summarization_exploring/summarize.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import random, time
+random.seed(42)
+
+from langchain.chains.summarize import load_summarize_chain
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+import load_optimism
+
+def summarize_thread(thread, model_name, chain_type):
+    if model_name in chat_models_openai:
+        llm = ChatOpenAI(temperature=0, model_name=model_name)
+    elif model_name in chat_models_anthropic:
+        llm = ChatAnthropic(temperature=0, model_name=model_name)
+
+    chain = load_summarize_chain(llm, chain_type=chain_type)
+    result = chain.invoke([thread])
+
+    return result
+
+st.title("Summarize")
+
+forum_path = "../../data/002-governance-forum-202406014/dataset/_out.jsonl"
+posts = load_optimism.ForumPostsProcessingStrategy.process_document(forum_path)
+df_posts = pd.DataFrame(posts).T
+
+st.write("Collected Posts")
+st.write(df_posts)
+
+threads = load_optimism.ForumPostsProcessingStrategy.return_threads(df_posts)
+minimum_posts = st.number_input("Minimum number of posts in a thread", 1, 20, 10)
+threads = [t for t in threads if t.metadata["num_posts"] >= minimum_posts]
+num_threads = st.number_input("Number of Threads to explore", 1, 20, 3)
+threads = random.sample(threads, num_threads)
+
+chat_models_openai = ["gpt-3.5-turbo-0125", "gpt-4o"]
+chat_models_anthropic = ["claude-3-sonnet-20240229", "claude-3-haiku-20240307", "claude-3-opus-20240229", "claude-3-5-sonnet-20240620"]
+chat_models = chat_models_openai + chat_models_anthropic
+model_name = st.multiselect("Model Name", chat_models, default=["gpt-4o", "claude-3-sonnet-20240229"])
+
+chain_type = st.multiselect("Chain Type", ["stuff", "map_reduce", "refine"], default=["stuff"])
+
+if st.button("Summarize Threads"):    
+    st.write("## Summarized Threads")
+    for thread in threads:
+        st.write("----")
+        st.write(thread.metadata)
+        st.write(thread.metadata["url"])
+        for m in model_name:
+            for c in chain_type:
+                st.write(f"### Model: {m}, Chain: {c}")
+                start = time.time()
+                result = summarize_thread(thread, m, c)
+                end = time.time()
+                st.write(result['output_text'])
+                st.write(f"(Time taken: {end-start}s)")
+
