82014395f851a6eee00f0ff15eb28b5fe4c13c2b
luizakp <luiza.puccinelli06@gmail.com>
2024-07-22 18:08:02 -0300

    chore: add octagram to filter

--- www/src/components/forum/table/filter-select.tsx
+++ www/src/components/forum/table/filter-select.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import {
   Select,
   SelectContent,
@@ -9,15 +8,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { Octagram } from "../Octagram";
+import { CATEGORY_COLORS } from "../categoryColors";
 
 interface FilterSelectProps {
   data: {
     label: string;
     options: { label: string; value: string }[];
   };
+  categoryColor?: string; // Add this prop to pass the color
 }
 
-export function FilterSelect({ data }: FilterSelectProps) {
+export function FilterSelect({ data, categoryColor }: FilterSelectProps) {
   return (
     <Select>
       <SelectTrigger className="w-full bg-muted focus:ring-0 focus:ring-offset-0">
@@ -25,10 +28,21 @@ export function FilterSelect({ data }: FilterSelectProps) {
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
-          <SelectLabel>{data.label}</SelectLabel>
+          <SelectLabel className="flex items-center gap-2">
+            {data.label}
+          </SelectLabel>
           {data.options.map((item) => (
             <SelectItem key={item.value} value={item.value}>
-              {item.label}
+              <div className="flex items-center gap-x-2">
+                <Octagram
+                  className={cn(
+                    "size-4 fill-current",
+                    //@ts-ignore
+                    CATEGORY_COLORS[item.label]
+                  )}
+                />
+                {item.label}
+              </div>
             </SelectItem>
           ))}
         </SelectGroup>
