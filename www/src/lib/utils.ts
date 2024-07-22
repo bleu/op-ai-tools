03461437deb5241303768bc13dce90bfb4b27fab
luizakp <luiza.puccinelli06@gmail.com>
2024-07-22 17:10:50 -0300

    chore: format date

--- www/src/lib/utils.ts
+++ www/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: Date | string | number | undefined) {
+  if (!date) return "";
+  return Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(date));
+}
