ca825154f8c9e807fd0d8ee5533b573591bee5eb
José Ribeiro <me@joseribeiro.dev>
2024-08-16 10:40:15 -0300

    move __main__ script to standalone summarizer example script

--- pkg/op-brains/op_brains/summarizer/summarizer.py
+++ pkg/op-brains/op_brains/summarizer/summarizer.py
@@ -42,22 +42,3 @@ def summarize_thread(url: str, model_name: str, use_mock_data: bool = False) -> 
     return llm.invoke(
         Prompt.default_summarizer.format(THREAD_CONTENT=thread.page_content)
     ).dict()
-
-
-if __name__ == "__main__":
-    urls = [
-        "https://gov.optimism.io/t/special-voting-cycle-9a-grants-council/4198",
-        "https://gov.optimism.io/t/airdrop-1-feedback-thread/80",
-        "https://gov.optimism.io/t/retro-funding-4-onchain-builders-round-details/7988",
-        "https://gov.optimism.io/t/the-future-of-optimism-governance/6471",
-        "https://gov.optimism.io/t/optimism-community-call-recaps-recordings-thread/6937",
-        "https://gov.optimism.io/t/how-to-start-a-project-at-optimism/7220",
-        "https://gov.optimism.io/t/grant-misuse-reporting-process/7346",
-        "https://gov.optimism.io/t/how-to-stay-up-to-date/6124",
-    ]
-    model_name = "gpt-4o"
-    for url in urls:
-        out = summarize_thread(url, model_name)
-        for key, value in out.items():
-            print(f"{key}: {value}")
-        print("-------")
