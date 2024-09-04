daa03d909c666fb0cc78ddb26f021e7618d5f8b7
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-04 17:40:51 -0300

    feat: refactor sidebar

---
+++ www/src/app/chat/layout.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import type React from "react";
+
+const MOCK_CHAT = [
+  "Chat Keyboard K Keyboareyboar Keyboar Chat keboardsewajieawjiewaeioawk",
+  "Chat Keyboard  KeyboarChat keboards",
+  "Chat Keyboard  Keyboarhat keboards",
+  "Chat Keyboard C Keyboarhat keboards",
+  "Chat Keyboard C Keyboarhat keboards",
+];
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <aside className="w-64 bg-chat-primary p-5">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold text-lg">Chats (5)</h2>
+          <div className="w-9 h-9 bg-optimism" />
+        </div>
+        <div className="mt-8 space-y-3">
+          {MOCK_CHAT.map((text, index) => (
+            <Button
+              variant="ghost"
+              className={`max-w-full overflow-hidden justify-start px-2 py-0 font-medium text-chat-secondary hover:text-optimism  ${index === 0 && "text-optimism bg-optimism/15 hover:bg-optimism/15"}`}
+              key={index}
+            >
+              {text}
+            </Button>
+          ))}
+        </div>
+      </aside>
+      <main className="">{children}</main>
+    </div>
+  );
+}
