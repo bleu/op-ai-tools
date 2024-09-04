f1b4fea02dbbc4089069c7a8e38c7c11e430b430
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-04 09:42:38 -0300

    chore: change hover location

--- www/src/components/chat/message/message.tsx
+++ www/src/components/chat/message/message.tsx
@@ -10,54 +10,54 @@ import { MessageAvatar } from "./message-avatar";
 import { MessageContent } from "./message-content";
 
 export interface MessageProps {
-  message: MessageType;
+	message: MessageType;
 }
 
 export const Message: React.FC<MessageProps> = ({ message }) => {
-  const [isEditable, setIsEditable] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
+	const [isEditable, setIsEditable] = useState(false);
+	const [isHovered, setIsHovered] = useState(false);
 
-  const handleEditClick = () => {
-    setIsEditable(true);
-  };
+	const handleEditClick = () => {
+		setIsEditable(true);
+	};
 
-  const isAnswer = message.name === "Optimism GovGPT";
+	const isAnswer = message.name === "Optimism GovGPT";
 
-  return (
-    <div
-      className={cn(
-        "flex flex-col gap-2 p-4",
-        isAnswer ? "items-start" : "items-end",
-      )}
-    >
-      <div
-        className={cn(
-          "flex gap-3 items-start",
-          isAnswer ? "flex-row" : "flex-row-reverse",
-        )}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <MessageAvatar name={message.name} />
-        <div className="flex flex-col">
-          <MessageContent
-            message={message}
-            isEditable={isEditable}
-            setIsEditable={setIsEditable}
-            isHovered={isHovered}
-          />
-        </div>
-        {!isAnswer && isHovered && (
-          <Button
-            variant="ghost"
-            className="px-0 mt-1"
-            size="sm"
-            onClick={handleEditClick}
-          >
-            <Pencil className="h-3.5 w-3.5" />
-          </Button>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={cn(
+				"flex flex-col gap-2 p-4",
+				isAnswer ? "items-start" : "items-end",
+			)}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			<div
+				className={cn(
+					"flex gap-3 items-start",
+					isAnswer ? "flex-row" : "flex-row-reverse",
+				)}
+			>
+				<MessageAvatar name={message.name} />
+				<div className="flex flex-col">
+					<MessageContent
+						message={message}
+						isEditable={isEditable}
+						setIsEditable={setIsEditable}
+						isHovered={isHovered}
+					/>
+				</div>
+				{!isAnswer && isHovered && (
+					<Button
+						variant="ghost"
+						className="px-0 mt-1"
+						size="sm"
+						onClick={handleEditClick}
+					>
+						<Pencil className="h-3.5 w-3.5" />
+					</Button>
+				)}
+			</div>
+		</div>
+	);
 };
