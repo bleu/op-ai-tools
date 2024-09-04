0009f61926e6f98a30e7a9279337561c24128c31
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-04 08:44:48 -0300

    feat: show message edit and message action only on hover

--- www/src/components/chat/message/message-content.tsx
+++ www/src/components/chat/message/message-content.tsx
@@ -14,11 +14,13 @@ import { MessageActions } from "./message-actions";
 export interface MessageContentProps {
   message: Message;
   isEditable: boolean;
+  isHovered: boolean;
   setIsEditable: (isEditable: boolean) => void;
 }
 export const MessageContent: React.FC<MessageContentProps> = ({
   message,
   isEditable,
+  isHovered,
   setIsEditable,
 }) => {
   const [editMessageContent, setEditMessageContent] = useState(
@@ -60,7 +62,7 @@ export const MessageContent: React.FC<MessageContentProps> = ({
   return (
     <div className={cn("p-3 rounded-md max-w-md overflow-hidden", "bg-accent")}>
       <FormattedMessage content={messageContent(message.data)} />
-      <MessageActions message={message} />
+      {isHovered && <MessageActions message={message} />}
     </div>
   );
 };
