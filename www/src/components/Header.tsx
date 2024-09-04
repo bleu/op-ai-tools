dd0848d57825f102502cad44847d30b8512c9f6d
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-04 11:37:33 -0300

    feat: insert header into App's layout

--- www/src/components/Header.tsx
+++ www/src/components/Header.tsx
@@ -1,11 +1,21 @@
-import { Search } from "lucide-react";
+"use client";
 
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
-import { MobileSidebar } from "./Sidebar";
+import { usePathname } from "next/navigation";
+import { MobileSidebar } from "./forum/Sidebar";
 
 export function Header() {
+  const pathname = usePathname();
+
+  let headerText = "";
+
+  if (pathname === "/") {
+    headerText = "GovGPT";
+  } else if (pathname.startsWith("/forum")) {
+    headerText = "GovSummarizer";
+  }
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <div className="md:hidden">
@@ -23,17 +33,8 @@ export function Header() {
             height={100}
             className="w-[100px] md:w-[150px]"
           />
-          <span className="text-xs md:text-sm font-light">GovSummarizer</span>
+          <span className="text-xs md:text-sm font-medium">{headerText}</span>
         </Link>
-        {/* <form className="w-1/2 hidden md:block">
-          <div className="relative">
-            <Search className="absolute right-3 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              className="w-full appearance-none bg-background pr-8 shadow-none rounded-2xl focus-visible:ring-"
-            />
-          </div>
-        </form> */}
       </div>
     </header>
   );
