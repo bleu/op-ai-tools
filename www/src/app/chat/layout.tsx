a798debe6d51893087185a861ce87786783c6f1b
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-10 17:55:32 -0300

    chore: remove unnecessary isMobile chcker

--- www/src/app/chat/layout.tsx
+++ www/src/app/chat/layout.tsx
@@ -2,7 +2,6 @@
 
 import { ChatHeader } from "@/components/chat/chat-header";
 import ChatSidebar from "@/components/chat/chat-sidebar";
-import useMobileStore from "@/states/use-mobile-state";
 import type React from "react";
 
 export default function ChatLayout({
@@ -10,13 +9,14 @@ export default function ChatLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { isMobile } = useMobileStore();
-
   return (
     <div className="flex-1">
       <ChatHeader />
       <div className="flex max-h-[calc(100dvh-4rem)] w-full overflow-hidden h-full">
-        {!isMobile && <ChatSidebar />}
+        <div className="hidden md:flex">
+        <ChatSidebar />
+        </div>
+
         <main className="flex flex-1">{children}</main>
       </div>
     </div>
